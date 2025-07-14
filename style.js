07a09ca86c6a720f5e0873bf1531f0b818bd2865
olaitancode <147386493+olaitancode@users.noreply.github.com>
2025-07-14 11:35:48 +0100

    bangees clone file

---
+++ style.js
@@ -0,0 +1,12 @@
+document.getElementById("im");
+let logo = document.getElementById("im")
+logo.addEventListener("click", Image
+
+)
+function Image() {
+    logo.setAttribute("src",  "./images/Frame-83-256x300.png")
+    logo.src = "./images/Frame-83-256x300.png"
+    // logo.innerHTML = 
+    // '<img src= "./images/Frame-83-256x300.png" alt="">'
+    
+}
